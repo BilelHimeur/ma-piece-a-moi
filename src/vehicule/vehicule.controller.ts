d8500b88322650ae9e2e11070d574b37b52f6689
BilelHimeur <bilelhimeur@gmail.com>
2024-02-21 14:47:01 +0100

    code refactoring

--- src/vehicule/vehicule.controller.ts
+++ src/vehicule/vehicule.controller.ts
@@ -11,13 +11,16 @@ export class VehiculeController {
     if (!vehicule) {
       console.log(`Véhicule n'existe pas en base de données`);
       // Recherche du véhicule par le biais du web scrapping, puis insertition du véducule en base de données
-      vehicule = await this.vehiclesService.scrapVehiculeDataFromAutoPieces(immatriculation);
-      if (vehicule) {
-        // Insérer les données du véhicule en base
-        await this.vehiclesService.createVehicule(vehicule);
-      } else {
-        return 'Aucun véhicule ne correspond à cette immatriculation dans AUTO PIECES !';
+      try {
+        vehicule = await this.vehiclesService.scrapVehiculeDataFromAutoPieces(immatriculation);
+        if (vehicule) {
+          // Véhicule trouvé sur le web, création en base
+          await this.vehiclesService.createVehicule(vehicule);
+        }
+      } catch {
+        console.log('Aucun véhicule ne correspond à cette immatriculation dans AUTO PIECES !');
       }
+      return vehicule;
     }
     return vehicule;
   }
