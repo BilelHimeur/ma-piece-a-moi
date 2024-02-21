fdeb68378bdd79e2e057c47f8be607f6f2290cf6
BilelHimeur <bilelhimeur@gmail.com>
2024-02-21 12:31:39 +0100

    Configuration Cross Origin pour activer le partage de resources avec le serveur front

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Configurer les options CORS pour accepter le partage des ressources avec le serveur front
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
+
 bootstrap();
